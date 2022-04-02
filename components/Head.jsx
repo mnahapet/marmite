25714ddbe1a998fa7322e53e033aac3edc981d00
mnahapet <mariam_nahapetyan@yahoo.com>
2022-04-02 13:41:53 +0200

    favicon

--- components/Head.jsx
+++ components/Head.jsx
@@ -10,7 +10,7 @@ const Head = ({ title }) => {
       <meta name="description" content="Free Web tutorials" />
       <meta name="keywords" content="HTML, CSS, JavaScript" />
       <meta name="author" content="Net Ninja | Huri Dev" />
-      <link rel="icon" href="/assets/img/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
 };
